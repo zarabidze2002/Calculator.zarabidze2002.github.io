adb927ba1d02d1399c44073bdda743575921aa22
zura arabidze <120923062+zarabidze2002@users.noreply.github.com>
2023-02-20 17:23:47 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+
+            let value = e.target.dataset.num;
+            screen.value += value;
+            console.log(typeof(screen.value));
+        })
+        console.log(screen.value);
+    });
+
+
+    equal.addEventListener('click', function(e){
+        if (screen.value === ''){
+            screen.value = "please enter";
+        }else{
+            let answer=eval(screen.value);
+            console.log(eval(screen.value));
+            screen.value = answer;
+        }
+    })
+
+    clear.addEventListener('click', function(e){
+        screen.value ="";
+    })
+
+
+})();
